d0b8cd8ec146f176a8a9866f856b504a5a676a06
“Pallavikawaregit config --global user.name “Pallavikaware <pallukaware22@gmail.com>
2022-10-14 15:35:42 +0530

    ArrayList program is added

---
+++ GitDemo1/src/GitDemo1/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package GitDemo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String args[]) {
+   	 ArrayList<String> list =new ArrayList<String>();
+   	    list.add("Pallavi");
+   	    list.add("Ram");
+   	    list.add("Anu");
+   	    
+   	    System.out.println(list);
+   }
+}
