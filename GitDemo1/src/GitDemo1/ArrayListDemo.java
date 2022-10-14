775d75d9a1eba54563151c5cfa205df89e69d595
“Pallavikawaregit config --global user.name “Pallavikaware <pallukaware22@gmail.com>
2022-10-14 16:38:14 +0530

    For each method is used

--- GitDemo1/src/GitDemo1/ArrayListDemo.java
+++ GitDemo1/src/GitDemo1/ArrayListDemo.java
@@ -3,12 +3,17 @@ package GitDemo1;
 import java.util.ArrayList;
 
 public class ArrayListDemo {
-	public static void main(String args[]) {
-   	 ArrayList<String> list =new ArrayList<String>();
+	 
+public static void main(String args[]) {
+   	 
+	ArrayList<String> list =new ArrayList<String>();
    	    list.add("Pallavi");
    	    list.add("Ram");
    	    list.add("Anu");
    	    
-   	    System.out.println(list);
+   	    
+   	    for(String s : list )
+   	    
+   	    System.out.println(s);
    }
 }
